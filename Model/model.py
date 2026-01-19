c0db7845de1b1572302541c0bdba4d20e8067539
RonaldCrack10 <rashidgaming6@gmail.com>
2026-01-19 20:45:45 +0100

    kernel_size and stride corrected

--- Model/model.py
+++ Model/model.py
@@ -6,7 +6,7 @@ class ClassifierCNN(nn.Module):
     def __init__(self):
         super(ClassifierCNN, self).__init__()
         self.conv1 = Conv2d(in_channels= 3, out_channels = 16, kernel_size= 3, padding = 1 ) # Input: 3 x 128 x 128, Output: 16 x 128 x 128
-        self.pool = MaxPool2d(kernel_size = 2, stride= 1)  # Output 1: 16 x 64 x 64 Output 2: 32 x 32 x 32 Output 3: 64 x 16 x 16
+        self.pool = MaxPool2d(kernel_size = 2, stride= 2)  # Output 1: 16 x 64 x 64 Output 2: 32 x 32 x 32 Output 3: 64 x 16 x 16
         self.conv2 = Conv2d(in_channels= 16, out_channels = 32, kernel_size= 3, padding = 1 ) # Output: 32 x 32 x 32
         self.conv3 = Conv2d(in_channels= 32, out_channels = 64, kernel_size= 3, padding = 1 ) # Output: 64 x 16 x 16
         self.flatten = Flatten()
